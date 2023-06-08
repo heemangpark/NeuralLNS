b29d44e80f1a89a4fc82e4d7de483b3aabde1d72
heemangpark <heemang0826@kaist.ac.kr>
2023-06-08 11:36:46 +0900

    .

--- temp.py
+++ temp.py
@@ -19,72 +19,76 @@ from utils.solver import solver
 
 
 def temp(cfg: dict):
-    seed_everything(cfg.seed)
-    exp_idx = list(range(cfg.num_data))
-    random.shuffle(exp_idx)
+    for num in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
+        seed_everything(cfg.seed)
+        exp_idx = list(range(cfg.num_data))
+        exp_id = list(random.sample(exp_idx, k=num))
+        gap = []
 
-    gap = []
-    for exp_id in tqdm(exp_idx[:100]):
-        grid, grid_graph, a_coord, t_coord = load_scenarios(
-            '{}{}{}_{}_{}/scenario_{}.pkl'.format(cfg.map_size, cfg.map_size, cfg.obs_ratio,
-                                                  cfg.num_agent, cfg.num_task, exp_id))
-        assign_idx, assign_coord = hungarian(a_coord, t_coord)
+        for exp_id in tqdm(exp_id):
+            grid, grid_graph, a_coord, t_coord = load_scenarios(
+                '{}{}{}_{}_{}/scenario_{}.pkl'.format(cfg.map_size, cfg.map_size, cfg.obs_ratio,
+                                                      cfg.num_agent, cfg.num_task, exp_id))
+            assign_idx, assign_coord = hungarian(a_coord, t_coord)
 
-        actual_init_cost, _ = solver(grid, a_coord, assign_coord, save_dir=cfg.save_dir, exp_name='init' + str(exp_id))
-        est_init_cost = sum([sum(t) for t in [[abs(a[0] - b[0]) + abs(a[1] - b[1]) for a, b, in zip(sch[:-1], sch[1:])]
-                                              for sch in assign_coord]])
+            actual_init_cost, _ = solver(grid, a_coord, assign_coord, save_dir=cfg.save_dir,
+                                         exp_name='init' + str(exp_id))
+            est_init_cost = sum(
+                [sum(t) for t in [[abs(a[0] - b[0]) + abs(a[1] - b[1]) for a, b, in zip(sch[:-1], sch[1:])]
+                                  for sch in assign_coord]])
 
-        prev_cost = est_init_cost
-        for itr in range(cfg.itrs):
-            temp_assign_idx = copy.deepcopy(assign_idx)
-            removal_idx = removal(assign_idx, t_coord)
-            removed = [False for _ in removal_idx]
-            for schedule in temp_assign_idx:
-                for i, r in enumerate(removal_idx):
-                    if removed[i]:
-                        continue
-                    if r in schedule:
-                        schedule.remove(r)
-                        removed[i] = True
+            prev_cost = est_init_cost
+            for itr in range(cfg.itrs):
+                temp_assign_idx = copy.deepcopy(assign_idx)
+                removal_idx = removal(assign_idx, t_coord)
+                removed = [False for _ in removal_idx]
+                for schedule in temp_assign_idx:
+                    for i, r in enumerate(removal_idx):
+                        if removed[i]:
+                            continue
+                        if r in schedule:
+                            schedule.remove(r)
+                            removed[i] = True
 
-            while len(removal_idx) != 0:
-                f_val = f_ijk(a_coord, t_coord, temp_assign_idx, removal_idx)
-                regrets = np.stack(list(f_val.values()))
-                argmin_regret = np.argmin(regrets, axis=None)
-                min_regret_idx = np.unravel_index(argmin_regret, regrets.shape)
-                r_idx, insertion_edge_idx = min_regret_idx
-                re_ins = removal_idx[r_idx]
-                ag_idx = 0
-                while True:
-                    ag_schedule = assign_idx[ag_idx]
-                    if insertion_edge_idx - (len(ag_schedule) + 1) < 0:
-                        ins_pos = insertion_edge_idx
-                        break
-                    else:
-                        insertion_edge_idx -= (len(ag_schedule) + 1)
-                        ag_idx += 1
+                while len(removal_idx) != 0:
+                    f_val = f_ijk(a_coord, t_coord, temp_assign_idx, removal_idx)
+                    regrets = np.stack(list(f_val.values()))
+                    argmin_regret = np.argmin(regrets, axis=None)
+                    min_regret_idx = np.unravel_index(argmin_regret, regrets.shape)
+                    r_idx, insertion_edge_idx = min_regret_idx
+                    re_ins = removal_idx[r_idx]
+                    ag_idx = 0
+                    while True:
+                        ag_schedule = assign_idx[ag_idx]
+                        if insertion_edge_idx - (len(ag_schedule) + 1) < 0:
+                            ins_pos = insertion_edge_idx
+                            break
+                        else:
+                            insertion_edge_idx -= (len(ag_schedule) + 1)
+                            ag_idx += 1
 
-                temp_assign_idx[ag_idx].insert(ins_pos, re_ins)
-                removal_idx.remove(re_ins)
-                assign_coord = [np.array(t_coord)[schedule].tolist() for schedule in temp_assign_idx]
-                est_cost = sum([sum(t) for t in [[abs(a[0] - b[0]) + abs(a[1] - b[1])
-                                                  for a, b, in zip(sch[:-1], sch[1:])] for sch in assign_coord]])
+                    temp_assign_idx[ag_idx].insert(ins_pos, re_ins)
+                    removal_idx.remove(re_ins)
+                    assign_coord = [np.array(t_coord)[schedule].tolist() for schedule in temp_assign_idx]
+                    est_cost = sum([sum(t) for t in [[abs(a[0] - b[0]) + abs(a[1] - b[1])
+                                                      for a, b, in zip(sch[:-1], sch[1:])] for sch in assign_coord]])
 
-            if (est_cost < prev_cost) or (itr == (cfg.itrs - 1)):
-                prev_cost = est_cost
-                assign_idx = temp_assign_idx
+                if (est_cost < prev_cost) or (itr == (cfg.itrs - 1)):
+                    prev_cost = est_cost
+                    assign_idx = temp_assign_idx
 
-        actual_final_cost, _ = solver(grid, a_coord, assign_coord, save_dir=cfg.save_dir, exp_name='fin' + str(exp_id))
-        if os.path.exists(cfg.save_dir):
-            shutil.rmtree(cfg.save_dir)
+            actual_final_cost, _ = solver(grid, a_coord, assign_coord, save_dir=cfg.save_dir,
+                                          exp_name='fin' + str(exp_id))
+            if os.path.exists(cfg.save_dir):
+                shutil.rmtree(cfg.save_dir)
 
-        perf = (actual_init_cost - actual_final_cost) / actual_init_cost * 100
-        gap.append(perf)
+            perf = (actual_init_cost - actual_final_cost) / actual_init_cost * 100
+            gap.append(perf)
 
-    plt.plot(gap)
-    plt.title('{:.4f}'.format(np.mean(gap)))
-    plt.savefig('itrs_{}.png'.format(cfg.itrs))
-    plt.clf()
+        plt.plot(gap)
+        plt.title('{:.4f}'.format(np.mean(gap)))
+        plt.savefig('itrs_{}.png'.format(cfg.itrs))
+        plt.clf()
 
 
 if __name__ == '__main__':
